8a35b48fe41fdbe5ba6d737f73fecd67e86688c5
EchoCharlie85 <martin.schweizer@bd.so.ch>
2017-05-02 14:50:35 +0200

    dummy

---
+++ db2db_step/src/main/java/dfg.java
@@ -0,0 +1,8 @@
+/**
+ * Created by bjsvwsch on 02.05.17.
+ */
+public class dfg {
+    public static void main(String[] args){
+
+    }
+}
---
+++ db2db_step/src/test/java/dfgTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by bjsvwsch on 02.05.17.
+ */
+public class dfgTest {
+
+}
