4f9bc3062f62a7017c2f8497a8a52d476220c7b4
EchoCharlie85 <martin.schweizer@bd.so.ch>
2017-05-10 15:06:56 +0200

    Logger überarbeitet

--- core/src/main/java/ch/so/agi/gretl/core/Logger.java
+++ core/src/main/java/ch/so/agi/gretl/core/Logger.java
@@ -3,8 +3,39 @@ package ch.so.agi.gretl.core;
 /**
  * Created by bjsvwsch on 09.05.17.
  */
-public interface Logger {
+public class Logger  {
+
+    private static final String INFO_NAME = "INFO: ";
+    private static final String DEBUG_NAME = "DEBUG: ";
     public static final int INFO_LEVEL = 1;
     public static final int DEBUG_LEVEL = 2;
-    void log(Integer LogLevel, String Message);
+
+    private Logger() {}
+
+
+    static public void log(int LogLevel, String Message) {
+        if (LogLevel== INFO_LEVEL) {
+            /** INFO **/
+            /**Write to a file**/
+            System.out.println(INFO_NAME+" "+Message);
+
+        }
+        else if (LogLevel== DEBUG_LEVEL) {
+            /** DEBUG **/
+            /**Write to another file**/
+            System.out.println(DEBUG_NAME+" "+Message);
+
+        }
+        else {
+            /**UNEXPECTED LOGLEVEL**/
+            /**Throws Exception**/
+            throw new RuntimeException("Logging-Fehler");
+        }
+
+
+    }
+
+    static public void log(int LogLevel, Exception e) {
+        log(LogLevel, e.getMessage());
+    }
 }
