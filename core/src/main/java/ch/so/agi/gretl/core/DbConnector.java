4b65ca4edee345f9ffbf0340cfe10cf6e50b15ff
barpastu <noemi.sturm@bd.so.ch>
2017-05-11 08:47:29 +0200

    DB-Connector angepasst

--- core/src/main/java/ch/so/agi/gretl/core/DbConnector.java
+++ core/src/main/java/ch/so/agi/gretl/core/DbConnector.java
@@ -1,5 +1,7 @@
 package ch.so.agi.gretl.core;
 
+import ch.so.agi.gretl.logging.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,26 +10,26 @@ import java.sql.SQLException;
  * Created by bjsvwsch on 02.05.17.
  */
 public class DbConnector {
-    private Connection con=null;
+    private static Connection con=null;
 
 
-
-     public Connection Con(String ConnectionUrl, String UserName, String Password) {
+    public static Connection Con(String ConnectionUrl, String UserName, String Password) {
         try {
             con = DriverManager.getConnection(
                     ConnectionUrl,UserName,Password);
             con.setAutoCommit(false);
+            Logger.log(Logger.DEBUG_LEVEL, "DB connected with these Parameters:  ConnectionURL:" + ConnectionUrl
+                        + " Username: " + UserName + " Password: " + Password);
         } catch (SQLException e) {
-            System.out.println(e);
             if (con!=null) {
                 try {
                     con.rollback();
                     con.close();
                 } catch (SQLException f) {
-                    System.out.println(f);
+                    Logger.log(Logger.INFO_LEVEL, f);
                 }
             }
-            throw new RuntimeException("Super Fehler", e);
+            throw new RuntimeException("Could not connect with database: ", e);
         };
         return con;
     }
