ac105eb7144f1cdef3c6996c5a70466eb7f25e13
barpastu <noemi.sturm@bd.so.ch>
2017-05-11 08:28:39 +0200

    DBConnector-Interface gelöscht

--- core/src/test/java/ch/so/agi/gretl/core/DbConnectorTest.java
+++ core/src/test/java/ch/so/agi/gretl/core/DbConnectorTest.java
@@ -5,28 +5,28 @@ import org.junit.Test;
 /**
  * Created by bjsvwsch on 03.05.17.
  */
-public class DbConnectorImpTest {
+public class DbConnectorTest {
     @Test
     public void con_pgsql() throws Exception {
-        DbConnectorImp x = new DbConnectorImp();
+        DbConnector x = new DbConnector();
         x.Con("jdbc:postgresql://10.36.54.198:54321/sogis", "barpastu", null);
     }
 
     @Test
     public void con_oracle() throws Exception {
-        DbConnectorImp x = new DbConnectorImp();
+        DbConnector x = new DbConnector();
         x.Con("jdbc:oracle:thin:@//srsofaioi18632.verw.rootso.org:1521/capi_p","SRVAGIGB", "");
     }
 
     @Test
     public void con_mssql() throws Exception {
-        DbConnectorImp x = new DbConnectorImp();
+        DbConnector x = new DbConnector();
         x.Con("jdbc:sqlserver://srsofaioi18943.verw.rootso.org:1433", "imdaspro_gis", "");
     }
 
     @Test
     public void con_sqlite() throws Exception {
-        DbConnectorImp x = new DbConnectorImp();
+        DbConnector x = new DbConnector();
         x.Con("jdbc:sqlite:./test/gemeindegrenzen.sqlite", null, null);
     }
 }
